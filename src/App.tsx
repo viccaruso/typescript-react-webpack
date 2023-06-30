6bb3c48912c5220a18cb2cc057ff2af6bbd56546
Victor Caruso <vic.caruso@gmail.com>
2023-06-29 20:32:58 -0700

    Set up base React app that utilizes Webpack,  Babel, and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Potato!</h1>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      // `js` and `jsx` files are parsed using `babel`
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // `ts` and `tsx` files are parsed using `ts-loader`
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  // pass all js and ts files through Babel
+  resolve: {
+    extensions: ['.*', '.js', '.jsx', '.ts', '.tsx'],
+  },
+};
